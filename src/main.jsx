72d686265498f5ff99390e5b01aa3b0b4c733840
Marcelo Moreira <profissionalsmbr@gmail.com>
2023-02-24 14:12:15 -0300

    products section

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import { Router } from './routes';
+
+library.add(fab, faCircleCheck);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
